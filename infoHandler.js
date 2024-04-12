68796917fb40f3d3006ed50830f23f6c3df6fbce
Timur Subasic <subasictimur@gmail.com>
2024-04-12 20:01:45 +0200

    Dodan info knjiga

--- infoHandler.js
+++ infoHandler.js
@@ -1,10 +1,10 @@
-$("#benefit-btn").click(function() {
+$(".benefit-btn").click(function() {
     $('html, body').animate({
         scrollTop: $("#benefites").offset().top
     }, 300); // Change the duration as needed
   });
 
-  $("#price-btn").click(function() {
+  $(".price-btn").click(function() {
     $('html, body').animate({
         scrollTop: $("#price").offset().top
     }, 300); // Change the duration as needed
@@ -22,8 +22,24 @@ $("#benefit-btn").click(function() {
     }, 300); // Change the duration as needed
   });
 
-  $("#use-btn").click(function() {
+  $(".use-btn").click(function() {
     $('html, body').animate({
         scrollTop: $("#use").offset().top
     }, 300); // Change the duration as needed
+  });
+
+
+  var stickyElement = $('.sticky-element');
+  var stickyElementOffset = $("#use").offset().top;
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= stickyElementOffset) {
+      stickyElement.addClass('sticky');
+      stickyElement.removeClass("gone");
+    } else {
+      stickyElement.removeClass('sticky');
+      stickyElement.addClass("gone");
+    }
   });
