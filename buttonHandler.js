6b72cda9aa1146615a060f4d0c3fdf966eadc368
Timur Subasic <subasictimur@gmail.com>
2024-04-12 20:34:34 +0200

    glavna naslov fix

--- buttonHandler.js
+++ buttonHandler.js
@@ -3,7 +3,7 @@
 $("#btn-one").click(function(){
     if($("#btn-one").text() == "Šta je Shilajit?"){
         $("#para-info").text("Shilajit(Mumija, Moomiyo, Mummiyo) je prirodni supstrat koji se sastoji od slojeva organskog materijala i minerala, kompresovanih kroz stoljeća u stijenama visokih planinskih lanaca poput Himalaja. Ovaj tamni, smolasti materijal poznat je po svojoj bogatoj koncentraciji fulvinske kiseline i više od 85 minerala u jonskom obliku, što ga čini izuzetno nutritivno bogatim.");
-        $("#btn-one").text("Beneficije");
+        $("#btn-one").text("Benefiti");
         $("#btn-two").text("Zašto?");
         $("#hero-naslov").text("Šta je Shilajit");
         $("#hero img").attr("src", "images/stae-shila.png");
@@ -12,7 +12,7 @@ $("#btn-one").click(function(){
         $("#para-info").text("Tradicionalno se cijeni u ayurvedskoj medicini zbog svojih adaptogenih svojstava, sposobnosti poboljšanja energije, vitalnosti i fizičkog oporavka, kao i podrške za mentalne funkcije. Shilajit se koristi i za unapređenje općeg zdravlja, jačanje imunološkog sistema, poboljšanje seksualnog zdravlja, te kao sredstvo za balansiranje šećera u krvi i smanjenje upalnih procesa u tijelu.");
         $("#btn-one").text("Šta je Shilajit?");
         $("#btn-two").text("Zašto?");
-        $("#hero-naslov").text("Benificije Shilajita");
+        $("#hero-naslov").text("Benefiti Shilajita");
         $("#hero img").attr("src", "images/benefit-shila.png");
     }
 });
@@ -23,7 +23,7 @@ $("#btn-two").click(function(){
         $("#para-info").text("Obzirom da je potentan i moćan, čisti shilajit će biti vaša prva doza za bolji i zdraviji način života. Forma smole je najčišća forma shilajita i dolazi sa visokih stijena Himalaja. Ne sadrži nikakve hemikalije niti dodatne sastojke. Ukoliko želite da znate ostale informacije o ovom nevjerovatnom proizvodu kao što je način upotrebe, ostali benefiti i cijena možete da pritisnete ");
         $("#para-info").append("<a class=' btn btn-outline-dark rounded-pill ' href= ' info.html '>ovdje.</a>");
         $("#btn-one").text("Šta je Shilajit?");
-        $("#btn-two").text("Beneficije");
+        $("#btn-two").text("Benefiti");
         $("#hero-naslov").text("Zašto čisti shilajit");
         $("#hero img").attr("src", "images/zasto-shila.png");
     }
@@ -31,7 +31,7 @@ $("#btn-two").click(function(){
         $("#para-info").text("Tradicionalno se cijeni u ayurvedskoj medicini zbog svojih adaptogenih svojstava, sposobnosti poboljšanja energije, vitalnosti i fizičkog oporavka, kao i podrške za mentalne funkcije. Shilajit se koristi i za unapređenje općeg zdravlja, jačanje imunološkog sistema, poboljšanje seksualnog zdravlja, te kao sredstvo za balansiranje šećera u krvi i smanjenje upalnih procesa u tijelu.");
         $("#btn-one").text("Šta je Shilajit?");
         $("#btn-two").text("Zašto?");
-        $("#hero-naslov").text("Benificije Shilajita");
+        $("#hero-naslov").text("Benefiti Shilajita");
         $("#hero img").attr("src", "images/benefit-shila.png");
     }
 });
